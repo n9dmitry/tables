b671ebd0a8d7ddbe0829f710f207608bb6267dbc
dev <nikitindmitry7@gmail.com>
2024-09-13 15:44:03 +0300

    v002.

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
